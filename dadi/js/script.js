e62f7ef29a2a477349e667feb70994fe970a10c0
domenicolafroscia <domenicolafroscia@gmail.com>
2023-10-23 16:42:40 +0200

    add: termine esercizio dadi

---
+++ dadi/js/script.js
@@ -0,0 +1,24 @@
+// Dati
+document.querySelector('#genera').addEventListener('click', function () {
+
+    const userNumber = Math.floor(Math.random() * 100);
+    const pcNumber = Math.floor(Math.random() * 100);
+
+    console.log(userNumber, pcNumber);
+
+    document.getElementById("casuale-1").innerHTML = "Numero giocatore: " + userNumber;
+
+    document.getElementById("casuale-2").innerHTML = "Numero PC: " + pcNumber;
+
+    // Logica
+    let message = 0;
+
+    if (userNumber > pcNumber) {
+        message = "Il vincitore è il giocatore."
+    } else if (pcNumber > userNumber) {
+        message = "Il vincitore è il PC."
+    }
+
+    // Output
+    document.getElementById("message").innerHTML = message;
+});
